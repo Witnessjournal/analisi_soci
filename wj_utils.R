1d3e2a30dc9304c955767abbf138f29320568573
Paolo Bosetti <paolo.bosetti@unitn.it>
2025-05-02 15:33:24 +0200

    Support for secret tokens

--- wj_utils.R
+++ wj_utils.R
@@ -1,5 +1,15 @@
 library(httr2)
 
+wj_tokens <- function(tokens_file = "winddoc_tokens.R") {
+  if (file.exists(tokens_file)) {
+    source("winddoc_tokens.R")
+  } else {
+    tokens <- list(token = Sys.getenv("WJ_TOKEN"),
+                   token_app = Sys.getenv("WJ_TOKEN_APP"))
+  }
+  return(tokens)
+}
+
 wj_paged_associates <- function(tokens, page=1, url = "https://app.winddoc.com/v1/api_json.php") {
   soci <- request(url) |>
     req_method("POST") |> 
@@ -31,4 +41,50 @@ wj_list_associates <- function(tokens, url = "https://app.winddoc.com/v1/api_jso
   return(associates)
 }
 
+wj_list_associates_lazy <- memoise::memoise(
+  wj_list_associates, 
+  cache = cachem::cache_disk(max_age = 3600*24, dir="cache"))
 
+wj_data <- function(tokens, url = "https://app.winddoc.com/v1/api_json.php") {
+  wj_list_associates_lazy(tokens) %>%
+    select(
+      nome,
+      cognome,
+      codice_fiscale,
+      data_iscrizione,
+      data_nascita = contatto_data_nascita,
+      contatto_indirizzo_citta,
+      contatto_indirizzo_cap,
+      contatto_indirizzo_provincia,
+      contatto_indirizzo_nazione,
+      data_iscrizione,
+      data_scadenza_quota,
+      deve_rinnovare
+    ) %>%
+    rename_with(~ str_replace_all(.x, "contatto_indirizzo_", ""),
+                everything())
+}
+
+wj_soci <- function(data) {
+  province <- read_tsv("province.txt", na = c(""))
+  data %>% 
+    mutate(
+      età = interval(ymd(data_nascita), today()) %/% years(1),
+      provincia = str_to_upper(provincia) %>% 
+        str_replace_all("[\\s]+$", "") %>% 
+        str_replace_all("[\\s]", "-")
+    ) %>% 
+    relocate(età, .after = data_nascita) %>%
+    left_join(
+      province, 
+      by = c("provincia" = "PROVINCIA")
+    ) %>% 
+    mutate(
+      provincia = ifelse(is.na(SIGLA), provincia, SIGLA),
+      citta = str_to_title(citta),
+      genere_n = str_sub(codice_fiscale, 10, 11) %>% as.numeric(),
+      genere = ifelse(genere_n>30, "F", "M")
+    ) %>% 
+    select(-c(SIGLA, REGIONE, genere_n)) %>% 
+    relocate(genere, .after=età)
+}
